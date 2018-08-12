ff8f0354e301491c2373a00acf9b251059ab204e
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-08-11 23:27:14 -0700

    Build client for production

--- server/index.ts
+++ server/index.ts
@@ -1,87 +1,18 @@
-#!/usr/bin/env node
+import * as express from 'express'
+import * as path from 'path'
 
-/**
- * Module dependencies.
- */
+const app = express()
 
-var app = require('../server/app')
-var debug = require('debug')('server:server')
-var http = require('http')
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/../../client/build')))
 
-/**
- * Get port from environment and store in Express.
- */
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  return res.sendFile(path.join(__dirname + '/../../client/build/index.html'))
+})
 
-var port = normalizePort(process.env.PORT || '5000')
-app.set('port', port)
+const port = process.env.PORT || 5000
+app.listen(port)
 
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app)
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port)
-server.on('error', onError)
-server.on('listening', onListening)
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val: string) {
-  var port = parseInt(val, 10)
-
-  if (isNaN(port)) {
-    // named pipe
-    return val
-  }
-
-  if (port >= 0) {
-    // port number
-    return port
-  }
-
-  return false
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error: {code: string; syscall: string}) {
-  if (error.syscall !== 'listen') {
-    throw error
-  }
-
-  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges')
-      process.exit(1)
-      break
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use')
-      process.exit(1)
-      break
-    default:
-      throw error
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address()
-  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
-  debug('Listening on ' + bind)
-  console.log(`Listening at http://localhost:${addr.port}`)
-}
+console.log(`Password generator listening on ${port}`)
