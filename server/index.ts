ea3be51560e2495b4c1ce210153178554ed0ccd7
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-08-12 15:21:34 -0700

    Move client build under build/client

--- server/index.ts
+++ server/index.ts
@@ -3,13 +3,13 @@ import * as path from 'path'
 
 const app = express()
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/../../client/build')))
+// Serve static files from the React app, found under build/client
+app.use(express.static(path.join(__dirname, '../client')))
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  return res.sendFile(path.join(__dirname + '/../../client/build/index.html'))
+  return res.sendFile(path.join(__dirname + '../client/index.html'))
 })
 
 const port = process.env.PORT || 5000
