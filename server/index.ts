df71e15c5206bffa357d807728eb439a5632fa29
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-08-12 15:30:25 -0700

    Fix path to client directory

--- server/index.ts
+++ server/index.ts
@@ -4,12 +4,12 @@ import * as path from 'path'
 const app = express()
 
 // Serve static files from the React app, found under build/client
-app.use(express.static(path.join(__dirname, '../client')))
+app.use(express.static(path.join(__dirname, '/../client')))
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  return res.sendFile(path.join(__dirname + '../client/index.html'))
+  return res.sendFile(path.join(__dirname + '/../client/index.html'))
 })
 
 const port = process.env.PORT || 5000
