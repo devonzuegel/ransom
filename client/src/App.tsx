8c476086eeb70ea036217c0c6c2eff109876b1e3
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-08-12 21:07:21 -0700

    Basic api for signing up and submitting a note

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,97 @@
+import * as DateFns from 'date-fns'
 import * as React from 'react'
-import * as http from './api/http'
+import {clearUser, getPersonInStorage, IPerson, setPersonInStorage} from './api'
 import './App.css'
 
-import logo from './logo.svg'
+const address = 'devon'
 
-class App extends React.Component<{}, {results: string[]}> {
+class Notes extends React.Component<{user: IPerson}> {
   public render() {
-    http.get('/foo').then(r => console.log({r}))
+    return (
+      this.props.user && (
+        <div>
+          {this.props.user.notes.map(note => (
+            <div>
+              <h2>
+                {DateFns.format(new Date(note.createdAt), 'D MMM YYYY, HH:mm')}
+              </h2>
+              <p>{note.content}</p>
+            </div>
+          ))}
+        </div>
+      )
+    )
+  }
+}
+
+class App extends React.Component<{}, {note: string; user: any}> {
+  public state = {note: '', user: undefined}
+
+  public render() {
+    const user = this.currentUser()
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h3>Write or paste your note here</h3>
+        <textarea autoFocus={true} onChange={this.onNoteChange} />
+        {!this.currentUser() && <button onClick={this.signup}>Sign up</button>}
+        <button onClick={this.persistNote}>Submit note</button>
+        {user && <Notes user={user} />}
+        <button onClick={this.clearStorage}>Clear storage</button>
+        <hr />
+        <pre style={{textAlign: 'left'}}>
+          {JSON.stringify({person: getPersonInStorage(address)}, null, 2)}
+        </pre>
       </div>
     )
+  }
+
+  public componentDidMount() {
+    this.updateView()
+  }
+
+  private updateView = () => {
+    const user = getPersonInStorage(address)
+    this.setState({user})
+  }
+
+  private signup = () => {
+    setPersonInStorage({address, notes: []})
+    this.updateView()
+  }
+
+  private currentUser = () => {
+    const retrieved = getPersonInStorage(address)
+    if (retrieved instanceof Error) {
+      return undefined
+    }
+    return retrieved
+  }
+  private onNoteChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    this.setState({note: event.target.value})
+  }
+
+  private persistNote = () => {
+    const retrieved = getPersonInStorage(address)
+    if (retrieved instanceof Error) {
+      return alert(retrieved)
+    }
+    const user = {
+      address,
+      notes: [
+        ...retrieved.notes,
+        {
+          content: this.state.note,
+          createdAt: new Date(),
+        },
+      ],
+    }
+    setPersonInStorage(user)
+    this.updateView()
+  }
+
+  private clearStorage = () => {
+    clearUser(address)
+    this.updateView()
   }
 }
 
---
+++ client/src/api/index.ts
@@ -0,0 +1,58 @@
+import {IObject, ParseError, Storage} from './Storage'
+
+export const setPersonInStorage = (person: IPerson) => {
+  Storage.set(person.address, person)
+}
+
+export const getPersonInStorage = (ethAddress: string) => {
+  const retrieved = Storage.get(ethAddress)
+  if (retrieved instanceof Error) {
+    return retrieved
+  }
+  return validatePersonObj(retrieved)
+}
+
+export const clearUser = (address: string) => {
+  Storage.clear(address)
+}
+
+/******************************************************************************************
+ ******************************************************************************************/
+
+export const validateNoteObj = (noteObj: IObject): INote | ParseError => {
+  if (typeof noteObj.content !== 'string') {
+    return new ParseError(`Invalid note content: ${noteObj.content}`)
+  }
+  const parsedDate = new Date(noteObj.createdAt)
+  if (!noteObj.createdAt || !(parsedDate instanceof Date)) {
+    return new ParseError(`Invalid note createdAt: ${noteObj.createdAt}`)
+  }
+  return {content: noteObj.content, createdAt: noteObj.createdAt}
+}
+
+export const validatePersonObj = (personObj: IObject) => {
+  // TODO: add more validations on the eth address
+  if (typeof personObj.address !== 'string') {
+    return new ParseError('Not a valid eth address')
+  }
+  if (!(personObj.notes instanceof Array)) {
+    return new ParseError('Invalid notes')
+  }
+  for (const note of personObj.notes) {
+    const error = validateNoteObj(note)
+    if (error instanceof Error) {
+      return error
+    }
+  }
+  return personObj as IPerson
+}
+
+export interface INote {
+  createdAt: Date
+  content: string
+}
+
+export interface IPerson {
+  address: string
+  notes: INote[]
+}
