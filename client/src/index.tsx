48ceb3f79871460d0930ebc375ad57555268d7dc
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-08-14 18:33:48 +0200

    localhost:8545 works

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -13,6 +13,7 @@ import * as React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
 import {persistStore} from 'redux-persist'
+import * as Web3 from 'web3'
 // // import {Ethereum} from './Ethereum'
 import store from './redux/store'
 
@@ -34,6 +35,22 @@ class PersistGate extends React.Component<{}, IPersistGateState> {
   public state: IPersistGateState = {rehydrating: true}
 
   public componentDidMount() {
+    const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+    // 'https://mainnet.infura.io/FE44zGZoLEQTXSTJ1XmS'
+    // const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+    console.log(web3)
+    console.log(web3.version.network)
+    console.log(web3.version)
+    console.log(web3.currentProvider.isMetaMask)
+    console.log(web3.currentProvider)
+    console.log(web3.net)
+    console.log(web3.eth.accounts[0])
+    web3.eth.getAccounts((err, accounts) => {
+      console.log(accounts)
+      console.log(JSON.stringify(accounts))
+    })
+    // console.log(web3.eth.getBalance(web3.eth.accounts[0]).toNumber())
+
     // // const newEthereum = new Ethereum(window.web3)
     // // this.setState(() => ({ethereum: newEthereum}))
     persistStore(store, {blacklist: []}, () => {
