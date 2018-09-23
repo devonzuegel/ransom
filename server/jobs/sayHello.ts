24e940fdff88e5d5393e302eca366d5c2a4c1483
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-09-23 14:00:00 -0700

    Replace console.log with serverLogger

--- server/jobs/sayHello.ts
+++ server/jobs/sayHello.ts
@@ -1,6 +1,7 @@
 import * as format from 'date-fns/format'
+import {serverLogger} from '@server/logger'
 
 export const sayHello = () => {
   const date = format(new Date(), 'HH:mm D MMM YYYY')
-  console.log(`[${date}]  Hello!`)
+  serverLogger.info(`[${date}]  Hello!`)
 }
