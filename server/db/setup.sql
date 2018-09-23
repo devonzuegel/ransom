25a9c59d9ea32a417a0e188e8a6bc6a69464c577
Devon Kristine Zuegel <devonzuegel@gmail.com>
2018-09-22 18:24:43 -0700

    Add notes table

--- server/db/setup.sql
+++ server/db/setup.sql
@@ -5,3 +5,13 @@ IF NOT EXISTS users
     address VARCHAR NOT NULL PRIMARY KEY,
     data jsonb
 );
+
+CREATE TABLE
+IF NOT EXISTS notes
+(
+    -- TODO: Validate this more strictly
+    "userAddress" VARCHAR NOT NULL references users(address),
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL,
+    "updatedAt" timestamp without time zone DEFAULT now() NOT NULL,
+    "content" text NOT NULL
+);
